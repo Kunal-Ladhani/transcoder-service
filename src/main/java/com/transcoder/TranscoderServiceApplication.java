52c72f1c14266c8185a1ad2d95182d5d62aa26d7
Kunal Ladhani <k.ladhani1@gmail.com>
2024-11-24 04:11:15 +0530

    printing access urls when starting application

--- src/main/java/com/transcoder/TranscoderServiceApplication.java
+++ src/main/java/com/transcoder/TranscoderServiceApplication.java
@@ -1,13 +1,34 @@
 package com.transcoder;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Collections;
+
+@Slf4j
 @SpringBootApplication
 public class TranscoderServiceApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(TranscoderServiceApplication.class, args);
-  }
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(TranscoderServiceApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("spring.profiles.default", "dev"));
+        Environment env = app.run(args).getEnvironment();
 
+        log.info("""
+                        \n
+                        Access URLs:
+                        --------------------------------------------------------
+                        \t\
+                        Local: \t\t http://{}:{}
+                        \t\
+                        External: \t http://localhost:{}
+                        --------------------------------------------------------""",
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getProperty("server.port"));
+    }
 }
