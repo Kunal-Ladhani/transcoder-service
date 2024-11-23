bd56049819f01cd3d8885d8a1d0a9830ad46a98a
Kunal Ladhani <k.ladhani1@gmail.com>
2024-11-23 21:48:43 +0530

    basic spring boot project setup

---
+++ src/test/java/com/transcoder/TranscoderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscoderServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
